97480747e2fa27adc3cf12ba40bcd8809c25797c
Lukáš Lánský <lukas@lansky.name>
2016-08-16 14:29:31 -0700

    Moving some logic from CLI to library

--- CSharpFormatting/CSharpFormatting.Cli/Program.cs
+++ CSharpFormatting/CSharpFormatting.Cli/Program.cs
@@ -1,9 +1,5 @@
-﻿using CSharpFormatting.Common.Chunk;
-using CSharpFormatting.Export.Html;
+﻿using CSharpFormatting.Library;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace CSharpFormatting.Cli
 {
@@ -17,88 +13,11 @@ namespace CSharpFormatting.Cli
                 return;
             }
             
-            var inputFile = System.IO.File.ReadAllLines(options.InputFile);
-            var codeLines = inputFile
-                .Select((line, i) => new CodeLine { Line = line, I = i })
-                .Where(t => t.Line.StartsWith("    "));
-
-            var codeBlocks = GetBlocks(codeLines);
-
-            var cSharpBlocks = codeBlocks
-                .Where(b => b.First().Line.Trim().ToLower() == "[lang=csharp]")
-                .Select(CleanBlock);
-
-            var toCompile = new StringBuilder();
-            int localI = 1;
-            foreach (var block in cSharpBlocks)
-            {
-                foreach (var line in block)
-                {
-                    Console.WriteLine($"{localI++}, {line.I}: {line.Line}");
-                    toCompile.AppendLine(line.Line);
-                }
-            }
-
-            var parser = new CSharpFormatting.Parsing.Roslyn.CSharpParser();
-            var annotationResult = parser.Parse(toCompile.ToString(), options.BaseReferencePath);
-
-            Console.WriteLine();
-            Console.WriteLine(annotationResult.DiagnosticResults.Count().ToString());
-
-            foreach (var r in annotationResult.DiagnosticResults)
-            {
-                Console.WriteLine(r.Message);
-            }
-
-            var exportedHtml = new HtmlExporter().ExportAnnotationResult(annotationResult.TextChunks.Cast<IChunk>());
-            System.IO.File.WriteAllText(options.OutputFile, exportedHtml);
-            
-            Console.ReadLine();
+            var formatter = new CSharpFormatter();
+            formatter.SaveHtmlForMarkdownFile(
+                options.InputFile,
+                options.OutputFile,
+                options.BaseReferencePath);
         }
-
-        private static List<List<CodeLine>> GetBlocks(IEnumerable<CodeLine> codeLines)
-        {
-            var codeBlocks = new List<List<CodeLine>>();
-            var previousLine = new CodeLine { I = -2 };
-
-            foreach (var codeLine in codeLines)
-            {
-                if (codeLine.I - 1 == previousLine.I) // same block
-                {
-                    codeBlocks.Last().Add(codeLine);
-                }
-                else // new block
-                {
-                    codeBlocks.Add(new List<CodeLine>());
-                    codeBlocks.Last().Add(codeLine);
-                }
-
-                previousLine = codeLine;
-            }
-
-            return codeBlocks;
-        }
-
-        private static List<CodeLine> CleanBlock(List<CodeLine> block)
-        {
-            var blockWithoutCSharp = block.Skip(1);
-
-            if (blockWithoutCSharp.First().Line.Trim().ToLower() == "[hide]")
-            {
-                blockWithoutCSharp = blockWithoutCSharp.Skip(1);
-            }
-
-            var blockWithoutLeadingSpaces = blockWithoutCSharp
-                .Select(line => new CodeLine { I = line.I, Line = line.Line.Substring(4) })
-                .ToList();
-
-            return blockWithoutLeadingSpaces;
-        }
-    }
-
-    struct CodeLine
-    {
-        public string Line;
-        public int I;
     }
 }
--- CSharpFormatting/CSharpFormatting.Library/CSharpFormatter.cs
+++ CSharpFormatting/CSharpFormatting.Library/CSharpFormatter.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using CSharpFormatting.Common.Chunk;
+using CSharpFormatting.Export.Html;
+using CSharpFormatting.Parsing.Roslyn;
+using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace CSharpFormatting.Library
 {
@@ -7,22 +12,55 @@ namespace CSharpFormatting.Library
     {
         public string GetHtmlForMarkdownContent(string mdContent, string baseReferencePath = null)
         {
-            return "";
+            var inputFile = mdContent.Replace("\r\n", "\n").Replace("\r", "\n").Split('\n');
+            var codeLines = inputFile
+                .Select((line, i) => new CodeLine { Line = line, I = i })
+                .Where(t => t.Line.StartsWith("    "));
+
+            var codeBlocks = MarkdownHelper.GetBlocks(codeLines);
+
+            var cSharpBlocks = codeBlocks
+                .Where(b => b.First().Line.Trim().ToLower() == "[lang=csharp]")
+                .Select(MarkdownHelper.CleanBlock);
+
+            var toCompile = new StringBuilder();
+            int localI = 1;
+            foreach (var block in cSharpBlocks)
+            {
+                foreach (var line in block)
+                {
+                    Console.WriteLine($"{localI++}, {line.I}: {line.Line}");
+                    toCompile.AppendLine(line.Line);
+                }
+            }
+
+            var parser = new CSharpParser();
+            var annotationResult = parser.Parse(toCompile.ToString(), baseReferencePath);
+
+            Console.WriteLine();
+            Console.WriteLine(annotationResult.DiagnosticResults.Count().ToString());
+
+            foreach (var r in annotationResult.DiagnosticResults)
+            {
+                Console.WriteLine(r.Message);
+            }
+
+            var exportedHtml = new HtmlExporter().ExportAnnotationResult(annotationResult.TextChunks.Cast<IChunk>());
+
+            return exportedHtml;
         }
 
         public string GetHtmlForMarkdownFile(string filePath, string baseReferencePath = null)
-        {
-            return GetHtmlForMarkdownContent(File.ReadAllText(filePath));
-        }
+            => GetHtmlForMarkdownContent(File.ReadAllText(filePath), baseReferencePath);
 
         public void SaveHtmlForMarkdownFile(string inputFilePath, string outputFilePath, string baseReferencePath = null)
         {
-            File.WriteAllText(outputFilePath, GetHtmlForMarkdownFile(inputFilePath));
+            File.WriteAllText(outputFilePath, GetHtmlForMarkdownFile(inputFilePath, baseReferencePath));
         }
 
         public void SaveHtmlForMarkdownContent(string mdContent, string outputFilePath, string baseReferencePath = null)
         {
-            File.WriteAllText(outputFilePath, GetHtmlForMarkdownContent(mdContent));
+            File.WriteAllText(outputFilePath, GetHtmlForMarkdownContent(mdContent, baseReferencePath));
         }
         
         public string GetHtmlForCsxContent(string csxContent, string baseReferencePath = null)
@@ -31,18 +69,16 @@ namespace CSharpFormatting.Library
         }
 
         public string GetHtmlForCsxFile(string filePath, string baseReferencePath = null)
-        {
-            return GetHtmlForCsxContent(File.ReadAllText(filePath));
-        }
+            => GetHtmlForCsxContent(File.ReadAllText(filePath), baseReferencePath);
 
         public void SaveHtmlForCsxFile(string inputFilePath, string outputFilePath, string baseReferencePath = null)
         {
-            File.WriteAllText(outputFilePath, GetHtmlForCsxFile(inputFilePath));
+            File.WriteAllText(outputFilePath, GetHtmlForCsxFile(inputFilePath, baseReferencePath));
         }
 
         public void SaveHtmlForCsxContent(string csxContent, string outputFilePath, string baseReferencePath = null)
         {
-            File.WriteAllText(outputFilePath, GetHtmlForCsxContent(csxContent));
+            File.WriteAllText(outputFilePath, GetHtmlForCsxContent(csxContent, baseReferencePath));
         }
     }
 }
